5a68fc0f6013ec52c347f7f659a12e04e53fb5ef
Colby Eagan <colbyeagan6@gmail.com>
2023-08-06 15:20:01 -0400

    Nav Bar Edits

--- js/scripts.js
+++ js/scripts.js
@@ -21,4 +21,13 @@ $(function() {
   });
 });
 
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 
